efddf4d6845247d9b57ba579ecedfad5bbfc6cc9
Bryan Seow <bryan04254@gmail.com>
2022-12-20 09:07:04 +0800

    merge resolve process

--- EDP/EDP/Models/Donation_Details.cs
+++ EDP/EDP/Models/Donation_Details.cs
@@ -1,6 +1,27 @@
-﻿namespace EDP.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace EDP.Models
 {
-	public class Donation_Details
+    [Keyless]
+    public class Donation_Details
 	{
-	}
+        [Required, Display(Name = "Waste Weight")]
+        public string waste_weight { get; set; } = string.Empty;
+
+        [Required, Display(Name = "Waste Image")]
+        public string waste_image { get; set; } = string.Empty;
+
+        [Required]
+        public bool collection_done { get; set; }
+
+        [Required]
+        public bool drop_off_done { get; set; }
+
+        [Required]
+        public string drop_off_point { get; set; } = string.Empty;
+
+        [Required]
+        public string user_donation_id { get; set; } = string.Empty;
+    }
 }
--- EDP/EDP/Models/Reviews.cs
+++ EDP/EDP/Models/Reviews.cs
@@ -1,6 +1,19 @@
-﻿namespace EDP.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EDP.Models
 {
 	public class Reviews
 	{
+		[Required, Key]
+		public string review_id { get; set; } = string.Empty;
+
+		[Required]
+		public string User_ID { get; set; } = string.Empty;
+
+		[Required]
+		public string product_id { get; set; } = string.Empty;
+
+		[Required]
+		public string review { get; set; } = string.Empty;
 	}
 }
